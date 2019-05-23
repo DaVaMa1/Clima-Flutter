ad5b9e819992bc6d47259722610094e6cf32e522
Daan van Marle <daanvanmarle@dsw.nl>
2019-05-23 15:52:28 +0200

    Appje niet af, moeilijk

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,9 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,15 +11,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    var location = Location();
+    await location.getLocation();
+    var data = await NetworkHelper(
+            'https://samples.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=b6907d289e10d714a6e88b30761fae22')
+        .getData();
+
+    await Future.delayed(Duration(seconds: 2), () => "");
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return new LocationScreen(data);
+    }));
+  }
+
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitChasingDots(
+          color: Colors.red,
         ),
       ),
     );
